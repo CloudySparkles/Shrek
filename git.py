f1211a8a85b11bc8bb2c086953441434cbcc9eba
CloudySparkles <cloudy078@gmail.com>
2018-07-26 17:51:27 +0000

    Shrektacular

--- git.py
+++ git.py
@@ -1,2 +1,5 @@
-git add
-git commit -a -m "my first git"
+from Bio import SeqIO
+reads=list(SeqIO.parse("/share/data/class/genomes/Ecoli-genome/MG1655-K12.fasta","fasta"))
+print len(reads)
+print reads[0].seq[1379:1479].translate()
+
